a7d08dac3b13f49cb08f9049b0fa1ad8e3b0b25d
SatwikaKacham <kachamsatwika@gmail.com>
2021-05-03 21:23:40 +0530

    feat: media quaries

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-
-
- var btnTrans = document.querySelector("#btn-translate");
+var btnTrans = document.querySelector("#btn-translate");
  var txtInput = document.querySelector("#text-input");
  var outPut = document.querySelector("#output");
-//  console.log(outPut);
-//  console.log(btnTrans);
-//  console.log(txtInput);
+ console.log(outPut);
+ console.log(btnTrans);
+console.log(txtInput);
  function urlLink(text){
   return  "https://api.funtranslations.com/translate/minion.json" +"?text=" + text
  }
